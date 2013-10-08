638cd1357bbf37f56cedcb6cf245e7066af0539b
Juan Antonio Marin <tonin@uco.es>
2013-10-08 14:30:56 +0200

    Version beta

--- checkhomes.py
+++ checkhomes.py
@@ -14,7 +14,12 @@ METHOD = "SIGU"
 QUERY_NT = "SELECT * FROM UT_CUENTAS_NT"
 
 lista_usuarios = []
+lista_nohomecifs = []
+
+lista_nocifshome = []
+lista_otrohome = []
 lista_nohome = []
+lista_noexist = []
 
 
 if METHOD == "AD":
@@ -64,18 +69,51 @@ procesados = 0
 
 for user in lista_usuarios:
     procesados = procesados + 1
-    if not (procesados % 1000): print "Procesados: ",procesados
+    if not (procesados % 10000): print "Procesados: ",procesados
     home = "\\\\cifs\\homescif$\\"+user
     if os.path.exists(home):       
         pass
     else:
-        lista_nohome.append(user)
+        lista_nohomecifs.append(user)
         
 print "Hay ",len(lista_usuarios)," personas"
-print "Hay ",len(lista_nohome)," personas que no tienen home"
+print "Hay ",len(lista_nohomecifs)," personas que no tienen home en homescif"
 
-print "Guardando la lista de usuarios"
+print "Comprobando homes alternativos"
+for user in lista_nohomecifs:
+    aduser=pyad.from_cn(user)
+    try:
+        home = aduser.get_attribute("HomeDirectory")
+        if u'\\\\CIFS\\HOME' in home:
+            lista_nocifshome.append(user)            
+        elif home == []:
+            lista_nohome.append(user)
+        else:
+            lista_otrohome.append(user)
+    except:
+            lista_noexist.append(user)
+        
+print "Resumen de resultados"
+print "Usuarios que no existen: ",len(lista_noexist)
+print "Usuarios con home distinto de homescif: ",len(lista_otrohome)
+print "Usuarios sin home definido: ",len(lista_nohome)
+print "Usuarios con home en homescif pero que no existe: ",len(lista_nocifshome)
+print "Usuarios con home en homescif y que si lo tienen: ",len(lista_usuarios)-len(lista_nohomecifs)
+
+print "Guardando las listas de usuarios"
 
 f = open("usuarios_sin_home.txt","w")
 f.write('\n'.join(lista_nohome))
+f.close()
 
+f = open("usuarios_no_existen.txt","w")
+f.write('\n'.join(lista_noexist))
+f.close()
+
+f = open("usuarios_home_distinto_homescif.txt","w")
+f.write('\n'.join(lista_otrohome))
+f.close()
+
+f = open("usuarios_homescifs_nocreado.txt","w")
+f.write('\n'.join(lista_nocifshome))
+f.close()
